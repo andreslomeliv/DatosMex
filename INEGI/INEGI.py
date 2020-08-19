11ec24c09eed6b0c7178c646080b8f92755f9f76
andreslomeliv <60153094+andreslomeliv@users.noreply.github.com>
2020-08-19 14:27:29 -0500

    Update INEGI.py

--- INEGI/INEGI.py
+++ INEGI/INEGI.py
@@ -12,4 +12,4 @@ class APItoken:
         self.token = token
         self.PIB = PIB(self.token)
     
-    localidades_dict = json.loads(Path('INEGIpy/localidades_INEGI.txt').read_text())
+    localidades_dict = json.loads(Path('localidades_INEGI.txt').read_text())
